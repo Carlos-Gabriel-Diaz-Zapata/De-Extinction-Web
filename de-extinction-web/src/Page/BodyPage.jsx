1f1c5bf8b1b22f3d21dcc2f484ec92dd881d0df1
GabrielDiazZapata <usr3566@salesianos-lacuesta.net>
2024-04-22 14:49:01 +0100

    feat: Création de cartes avec fonctions d'animation, ajustement des détails du site Web et correction de bugs dans le pied de page et le corps

--- de-extinction-web/src/Page/BodyPage.jsx
+++ de-extinction-web/src/Page/BodyPage.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-// import trailer from '../assets/trailer.mp4';
+import trailer3 from '../assets/Trailer3.mp4';
 
 const BodyPage = () => {
     return (
       <div>
         <div className="main-page">
-          {/* <video autoPlay loop muted className="background-video">
-            <source src={trailer} type="video/mp4"/>
-          </video> */}
+          <video autoPlay loop muted className="background-video">
+            <source src={trailer3} type="video/mp4"/>
+          </video>
           <div className="content">
             <h1 className='title' data-text="De-Extinction"></h1>
             <h3 className='subtitle'>¡Corre sin límites en un mundo donde la aventura nunca termina!</h3>
--- de-extinction-web/src/Page/HowToPlay.jsx
+++ de-extinction-web/src/Page/HowToPlay.jsx
@@ -1,41 +1,84 @@
 import React from 'react';
-import CardImage1 from '../assets/fondoDino.jpg';
-import CardImage2 from '../assets/fondoDino.jpg';
-import CardImage3 from '../assets/fondoDino.jpg';
-import CardImage4 from '../assets/fondoDino.jpg';
+import fosil from '../assets/fondoDino.jpg'; // Importa la imagen
 
 const HowToPlay = () => {
-    return (
-        <div className="how-to-play">
-            <h2>¿Cómo se juega?</h2>
-            <div className="cards-container">
-                <div className="card">
-                    <img src={CardImage1} alt="Card 1" />
-                    <div className="card-info">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </div>
-                </div>
-                <div className="card">
-                    <img src={CardImage2} alt="Card 2" />
-                    <div className="card-info">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </div>
-                </div>
-                <div className="card">
-                    <img src={CardImage3} alt="Card 3" />
-                    <div className="card-info">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </div>
-                </div>
-                <div className="card">
-                    <img src={CardImage4} alt="Card 4" />
-                    <div className="card-info">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </div>
-                </div>
+  return (
+    <div className="how-to-play">
+      <h2 className='title3'>¿Cómo se juega?</h2>
+      <div className="cards-container">
+        {/* Tarjeta 1 */}
+        <div className="card">
+          <div className="content">
+            <div className="back">
+              <div className="back-content">
+                <p> El objetivo principal del juego es guiar a un dinosaurio a través de un mundo prehistórico lleno de obstáculos mientras corre sin parar. El jugador debe saltar sobre obstáculos y evitar colisiones para sobrevivir el mayor tiempo posible y obtener la puntuación más alta.
+                Contenido:
+                "Guía al dinosaurio a través de un mundo prehistórico"
+                "Salta sobre obstáculos y evita colisiones"
+                "Sobrevive el mayor tiempo posible para obtener la puntuación más alta"</p>
+              </div>
             </div>
+            <div className="front">
+              <div className="front-content">
+                <div className="title2">
+                  <p className="card-footer"><strong>Objetivo del juego</strong></p>
+                </div>
+                <img src={fosil} alt="Imagen 1" /> {/* Añade la imagen */}
+              </div>
+            </div>
+          </div>
         </div>
-    );
+
+        {/* Tarjeta 2 */}
+        <div className="card">
+          <div className="content">
+            <div className="back">
+              <div className="back-content">
+                <p>"De-Extinction" es un juego de tipo "infinite runner" en el que el dinosaurio corre automáticamente. El jugador debe controlar los saltos del dinosaurio para evitar obstáculos y recolectar bonificaciones. Los controles son simples, con un solo botón para saltar.
+                Contenido:
+                "Dinosaurio corre automáticamente"
+                "Controla los saltos para evitar obstáculos"
+                "Recoge bonificaciones para mejorar tu puntuación"</p>
+              </div>
+            </div>
+            <div className="front">
+              <div className="front-content">
+                <div className="title2">
+                  <p className="card-footer"><strong>Mecánica de juego</strong></p>
+                </div>
+                <img src={fosil} alt="Imagen 2" /> {/* Añade la imagen */}
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Tarjeta 3 */}
+        <div className="card">
+          <div className="content">
+            <div className="back">
+              <div className="back-content">
+              <p>Para tener éxito en "De-Extinction",
+                es importante seguir algunos consejos clave.
+                Estos incluyen mantener la concentración, practicar el timing de los saltos
+                y aprender a reconocer patrones de obstáculos. 
+                Contenido:
+                "Mantén la concentración y la calma para tomar decisiones precisas"
+                "Practica el timing de los saltos para evitar obstáculos"
+                "Aprende a reconocer patrones de obstáculos para anticipar tus movimientos"</p>
+              </div>
+            </div>
+            <div className="front">
+              <div className="front-content">
+                <div className="title2">
+                  <p className="card-footer"><strong>Consejos para el jugador</strong></p>
+                </div>
+                <img src={fosil} alt="Imagen 3" /> {/* Añade la imagen */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HowToPlay;
