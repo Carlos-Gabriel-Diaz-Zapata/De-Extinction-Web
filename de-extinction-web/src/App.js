cf4293fbfe8f6eb9ca90ae20de0da7f6290dbc30
GabrielDiazZapata <usr3566@salesianos-lacuesta.net>
2024-05-06 08:11:37 +0100

    feat: Completed registration page, beginning of profile page

--- de-extinction-web/src/App.js
+++ de-extinction-web/src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 import PrincipalPage from './components/PrincipalPage'; 
 import LoginPage from './Page/LoginPage';
-import RegisterPage from '../src/Page/RegisterPage';
+import RegisterPage from '../src/Page/RegisterPage';  
+import Profile from './Page/Profile';
 function App() {
   return (
     <div className='App'>
@@ -11,6 +12,7 @@ function App() {
           <Route path='/' element={<PrincipalPage />} />
           <Route path='/login' element={<LoginPage />} />
           <Route path='/register' element={<RegisterPage />}/>
+          <Route path='/profile' element={<Profile/>}/>
         </Routes>
       </Router>
     </div>
--- de-extinction-web/src/layout/Header.jsx
+++ de-extinction-web/src/layout/Header.jsx
@@ -13,17 +13,21 @@ const Header = ({ onLoginButtonClick }) => {
     }
   };
   return (
-    <header className="header-container">
-      <div className="header-left">
-        <button className="header-button" onClick={() => scrollToSection('como-jugar')}>
+    <header className='header-container'>
+      <div className='header-left'>
+        <button className='header-button' onClick={() => scrollToSection('como-jugar')}>
           Como se Juega</button>
-        <button className="header-button" onClick={() => scrollToSection('somos-de-extinction')}>Somos De-Extinction</button>
-        <button className="header-button" onClick={onLoginButtonClick}>
+        <button className='header-button' onClick={() => scrollToSection('somos-de-extinction')}>Somos De-Extinction</button>
+        <button className='header-button' onClick={onLoginButtonClick}>
           Iniciar Sesión
         </button>
+        <button className='header-button'>Perfil</button>
+        <button className='header-button'>Puntuaciones</button>
+        <button className='header-button'>Administrador</button>
+        <button className='header-button'>Cerrar Cesión</button>
       </div>
-      <div className="header-right">
-        <img src={logo} alt="Logo" className="header-logo" />
+      <div className='header-right'>
+        <img src={logo} alt='Logo' className='header-logo' />
       </div>
     </header>
   );
